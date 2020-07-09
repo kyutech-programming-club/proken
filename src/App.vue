f4de9fe95de781bf508b13a8cab67a1e5611e8b1
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-07-09 22:45:43 +0900

    Create tabs

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
-import Home from '@/components/Home.vue'
+  import Header from '@/components/Header.vue'
+  import Home from '@/components/Home.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Home
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Home
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
