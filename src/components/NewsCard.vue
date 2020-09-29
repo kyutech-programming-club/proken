b160356a236a2046c87a3a54247e5e6fdfb57f74
o-xian999 <onoyama1999910@gmail.com>
2020-09-29 17:54:30 +0900

    Make NewsCard component

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <g-link :to="path">
+      <h3>{{ title }}</h3>
+  </g-link>
+</template>
+
+<script>
+export default {
+  props: {
+    path: String,
+    title: String
+  }
+}
+</script>>
